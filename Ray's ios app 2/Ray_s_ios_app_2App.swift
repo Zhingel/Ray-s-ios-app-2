4e2fbca0c5df68f004c943d46f0d308d42a87fbf
Stas <staszhingel@icloud.com>
2021-02-04 08:58:10 +0300

    Initial Commit

---
+++ Ray's ios app 2/Ray_s_ios_app_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ray_s_ios_app_2App.swift
+//  Ray's ios app 2
+//
+//  Created by Стас Жингель on 04.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Ray_s_ios_app_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
